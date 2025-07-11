399b91faa098e8f459dc9d227b00101f928797d2
Francogallardo99 <gal24016@byui.edu>
2025-07-10 21:56:20 -0300

    a

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,32 +1,29 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('');
-const li = document.createElement('')
-const deleteButton = document.createElement('button');
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-list.append(li);
+const list = document.querySelector('#chapterList');
 
 button.addEventListener("click", function() {
-    if (input.value.trim() === '') { 
+    if (input.value.trim() === '') {
         alert("Please enter a chapter.");
+        input.focus();
         return;
     } else {
+        
         const li = document.createElement('li');
         const deleteButton = document.createElement('button');
 
         li.textContent = input.value;
-        deleteButton.textContent = '❌';
+        deleteButton.textContent = ' ❌';
 
         li.append(deleteButton);
         list.append(li);
 
         deleteButton.addEventListener('click', function() {
             list.removeChild(li);
+            input.focus(); 
         });
 
-        input.value = ''; 
-        input.focus();
+            input.value = '';
+            input.focus();
     }
 });
