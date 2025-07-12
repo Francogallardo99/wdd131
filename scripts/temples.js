560a3fe05e7db697857014c62d7ca2d4ea1baf80
Francogallardo99 <gal24016@byui.edu>
2025-07-12 20:12:28 -0300

    Week02

---
+++ scripts/temples.js
@@ -0,0 +1,10 @@
+let date = new Date();
+let year = date.getFullYear();
+let span = document.getElementById("currentyear");
+if (span) {
+    span.textContent = year;
+    console.log("Year set:", year); 
+
+}
+let lastModifiedParagraph = document.getElementById('lastModified');
+lastModifiedParagraph.textContent = `Last Modified: ${document.lastModified}`;
