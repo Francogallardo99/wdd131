05117783797eddb230a04193d6355e3f49550b85
Francogallardo99 <gal24016@byui.edu>
2025-08-01 23:20:59 -0300

    Here we go again

--- scripts/form.js
+++ scripts/form.js
@@ -9,7 +9,33 @@ if (span) {
 let lastModifiedParagraph = document.getElementById('lastModified');
 lastModifiedParagraph.textContent = `Last Modified: ${document.lastModified}`;
 
-const products = ["Laptop", "Mouse", "Mousepad", "PenDrive", "Webcam" , "Headset", "Monitor", "Keyboard", "Printer", "Speakers" , "Smartphone", "Tablet", "Smartwatch", "External Hard Drive", "USB Hub", "Graphics Card", "Motherboard", "RAM", "Power Supply", "Cooling System"];
+const products = [
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
+];
 
 const selectElement = document.getElementById("products");
 
